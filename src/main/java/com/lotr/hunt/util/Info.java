9e35932ae7637ed8de5438c418ccfcd7b34cf798
ktsigkouni001 <konstantinos.tsigkounis@pwc.com>
2022-05-02 22:37:52 +0300

    Version correction

--- src/main/java/com/lotr/hunt/util/Info.java
+++ src/main/java/com/lotr/hunt/util/Info.java
@@ -5,6 +5,6 @@ package com.lotr.hunt.util;
  * @date 01/05/2022
  */
 public interface Info {
-    public static final String VERSION = "v0.0.1";
+    public static final String VERSION = "v1.0.0";
     public static final String CREATED_DATE = "01/05/2022";
 }
